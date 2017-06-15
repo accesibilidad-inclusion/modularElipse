1df39c45d95a53411a26b59468907e2686d4b59a
Herbert Spencer <herbert.spencer@gmail.com>
2017-06-15 18:20:25 -0400

    adj

--- modular.js
+++ modular.js
@@ -1,49 +1,99 @@
 /*
 
-  Modular Arithmetic
-  Visualizing time tables
-  @hspencer ~ 2016
+Modular Arithmetic
+Visualizing time tables
+@hspencer ~ 2016
 
 */
+
+var yellowDot = false; // draw the yellow dot?
+var prop, propHtml;
+
 
 
 function setup() {
   var myCanvas = createCanvas(700, 700);
   myCanvas.parent('modular');
   ellipseMode(CENTER);
+  propHtml = document.getElementById('yell');
 }
 
 function draw() {
   background(0);
+  prop = mouseX/width;
   var num = document.getElementById('num').value;
-  var mult = document.getElementById('mult').value
-  ct(width/2, height/2, height*.4, num, mult);
+  var mult = document.getElementById('mult').value;
+  ct(width / 2, height / 2, height * 0.44, num, mult);
 }
 
 function ct(x, y, r, num, mult) {
-  
-  var inc = TWO_PI/num;
-  var pr = map(inc, 0, PI, 2, r*.2); // point radius
-  pr = constrain(pr, 1, 40);
-
   push();
   translate(x, y);
   rotate(PI);
-  
-  // cicle of points
-  noStroke();
-  fill('#EA5B13');
-  for (var t = 0; t < TWO_PI; t+=inc) {
-    ellipse(cos(t)*r, sin(t)*r, pr, pr);
-  }
-  
+
+  var inc = TWO_PI / num;  // distance between points
+  var currentNumber = 0;
+
   // lines
-  var lineAlpha = map(num, 0, 600, 150, 30);
-  lineAlpha = constrain(lineAlpha, 25, 150);
-  stroke(255, 220, 228, lineAlpha);
+  var alpha = map(num, 0, 600, 110, 30); // alpha of the line
+  alpha = constrain(alpha, 25, 150);
+
   for (var i = 0; i < num; i++) {
     var result = (i * mult) % num;
-    line(cos(i*inc)*r, sin(i*inc)*r, cos(result*inc)*r, sin(result*inc)*r);
+
+    // origin
+    var x1 = cos(i * inc) * r;
+    var y1 = sin(i * inc) * r;
+
+    // result
+    var x2 = cos(result * inc) * r;
+    var y2 = sin(result * inc) * r;
+
+    // prop % to result
+    var xm = lerp(x1, x2, prop);
+    var ym = lerp(y1, y2, prop);
+
+    stroke(250, 222, 228, alpha);
+    strokeWeight(1);
+    line(x1, y1, x2, y2);
+
+    // calc current number
+    if(dist(mouseX-width/2, mouseY-height/2, x2, y2) < pr*4){
+      currentNumber = num;
+      ellipse(0, 0, 20, 20);
+    }else{
+      currentNumber = 0;
+    }
+
+    // draw yellow dot
+    if (yellowDot) {
+      stroke(255, 222, 0);
+      strokeWeight(3);
+      point(xm, ym);
+      propHtml.innerHTML = prop.toPrecision(3);
+    }else{
+      propHtml.innerHTML = "&hellip;";
+    }
+  }
+  
+  // draw current number
+  textFont("Helvetica");
+  textSize(48);
+  text(toString(t), x, y);
+
+  // cicle of points
+  noStroke();
+  fill(255, 60, 0);
+  var pr = map(inc, 0, PI, 2, r * 0.2); // point radius
+  pr = constrain(pr, 1, 40);
+  for (var t = 0; t < TWO_PI; t += inc) {
+    ellipse(cos(t) * r, sin(t) * r, pr, pr);
   }
   pop();
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    yellowDot = !yellowDot;
+  }
 }
